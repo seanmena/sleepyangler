588f77659d4041f883236ea3b1ca91c8b0910a16
Sean Mena <seanmena13@gmail.com>
2023-02-06 09:19:46 -0700

    added some styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Home.css";
 import fish from "../assets/images/IMG_0872.jpeg";
-import biglogo from "../assets/images/Beige horizontal border.png";
+import biglogo from "../assets/images/white-logo.png";
 
 export default function Home() {
   return (
     <div>
       <div className="hero">
         <img className="bkg-img" src={fish} alt="trout"></img>
-        <img className="big-logo" src={biglogo} alt="logo"></img>
+        <img className="big-logo fade-in" src={biglogo} alt="logo"></img>
       </div>
     </div>
   );
