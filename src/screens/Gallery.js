71029531bd4ed72b5c3cb2e970df5ee2df98b858
Sean Mena <seanmena13@gmail.com>
2023-12-29 09:22:12 -0700

    added photos to carousel

--- src/screens/Gallery.js
+++ src/screens/Gallery.js
@@ -1,11 +1,16 @@
 import React from "react";
 import picture from "../assets/fish.jpeg";
+import picture2 from "../assets/bigfish.png";
+import picture3 from "../assets/handfly.jpeg";
+import picture4 from "../assets/induhriv.png";
+import picture5 from "../assets/looking.jpeg";
+import picture6 from "../assets/portrait.png";
 import "./Gallery.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 const Gallery = () => {
-  let images = [picture, picture, picture, picture, picture, picture];
+  let images = [picture, picture2, picture3, picture4, picture5, picture6];
 
   return (
     <div className="gallery-container">
@@ -24,9 +29,6 @@ const Gallery = () => {
         </div>
         <div>
           <img className="gallery-img" src={images[4]} alt="fish!" />
-        </div>
-        <div>
-          <img className="gallery-img" src={images[5]} alt="fish!" />
         </div>
       </Carousel>
     </div>
